4343534e01b98c118fd1fd031120bbd1ec4f40a2
CodeMat <codemat.biz@gmail.com>
2022-11-19 14:34:56 +0100

    updated

---
+++ pages/gates.js
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const Gates = () => {
+  return (
+    <Box px='6' py='20'>
+      <Text textAlign='center' fontWeight='bold' fontSize='3xl'>
+        Coming Soon!
+      </Text>
+    </Box>
+  );
+};
+
+export default Gates;
