f39416f76c90046b6027692d5a1782f00a939b67
CodeMat <codemat.biz@gmail.com>
2022-11-20 02:43:07 +0100

    updated

--- components/NavHeader.js
+++ components/NavHeader.js
@@ -4,7 +4,7 @@ import MobileMenuBtn from "./menu/MobileMenuBtn";
 
 const NavHeader = () => {
   return (
-    <Box bg='gray.800' as='nav' px='6' py='4' shadow='md' pos='sticky' top='0' zIndex='60'>
+    <Box bg='gray.800' as='nav' p='4' shadow='md' pos='sticky' top='0' zIndex='60'>
       <HStack maxW='6xl' mx='auto' align='center' justify='space-between'>
         <Text
           bgGradient='linear(to-l, #7928CA, #FF0080)'
